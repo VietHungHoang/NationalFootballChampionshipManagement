31a3bb037754131c527fd38279b97745b843704f
Hung Hoang <Viethung18tuoi@gmail.com>
2024-11-23 15:46:44 +0700

    update

--- src/main/java/com/vhh/nfcm/entity/Card.java
+++ src/main/java/com/vhh/nfcm/entity/Card.java
@@ -1,15 +1,15 @@
-package com.vhh.nfcm.entity;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Entity
-@Table(name = "cards")
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
-public class Card {
-}
+//package com.vhh.nfcm.entity;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Table;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Entity
+//@Table(name = "cards")
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Data
+//public class Card {
+//}
